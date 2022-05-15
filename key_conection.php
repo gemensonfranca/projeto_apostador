c4956d8cc87f12f41943ac79aa798b8f7e035162
Gemenson França <gemensonfranca@outlook.com>
2022-05-15 15:53:13 -0300

    arquivo

---
+++ key_conection.php
@@ -0,0 +1,18 @@
+<?php
+
+// +------------------------------------------------------------------------+
+// | key_conection.php                                                      |
+// +------------------------------------------------------------------------+
+// | Copyright (c) Firegreen 2021. All rights reserved.                     |
+// | Version       1.00                                                     |
+// | Creation      31/05/2021                                               |
+// | Website       http://www.firegreen.com.br                              |
+// +------------------------------------------------------------------------+ 
+
+// LOCAL HOST
+// include "conexao/local_host.php";
+
+// WEB HOST
+include "conexao/web_host.php";
+
+?>
